92beea7167a138e4f9efb1fb1dc0d845395abcdb
YamanFattal <yamanfattal1108@gmail.com>
2024-07-16 12:42:27 +0200

    added getHumanChoice function

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function getComputerChoice (){
     let randNum = Math.random();
     if (0 <= randNum < 0.3){
-        return "Rock";
+        return "rock";
     }
 
     else if(0.3 <= randNum < 0.7){
-        return "Paper";
+        return "paper";
     }
     
     else {
-        return "Scissors";
+        return "scissors";
     }
+}
+
+function getHumanChoice (){
+   let choice = prompt("rock,paper or scissors?");
+    return choice;
 }
 
